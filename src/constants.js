9c8be392982a78f73a3f1993d96945229e77a5a5
Ahmed Chouihi <ahmedchouihi@gmail.com>
2024-04-12 22:13:43 +0100

    finish 2d portfolio animation

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my development environment. As a back-end developer, I leverage my experience with various languages like PHP, JavaScript (including frameworks like React.js), and content management systems (CMS) like WordPress. My current focus lies in building bridges between these technologies - for instance, integrating WordPress with frameworks like Laravel. This allows me to create powerful and dynamic web applications.
+    Here is my <a href="https://github.com/ahmedch1" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
+  You'll learn how I built this portfolio you're currently playing through!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently `,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. `,
+  resume: `This is my desk and on it is my resume`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library `,
+  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).`,
+  exit: `If you want to exit  just close the tab.`,
+};
